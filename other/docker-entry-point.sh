9c514f6d7ff098365be4f2008229f01d906eac2f
Good <35160768+itsmegood@users.noreply.github.com>
2024-07-24 07:20:48 +0530

    Update docker-entry-point.sh

--- other/docker-entry-point.sh
+++ other/docker-entry-point.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
 npx prisma migrate deploy
-sqlite3 /litefs/data/sqlite.db "PRAGMA journal_mode = WAL;"
-sqlite3 /litefs/data/cache.db "PRAGMA journal_mode = WAL;"
+# sqlite3 /litefs/data/sqlite.db "PRAGMA journal_mode = WAL;"
+# sqlite3 /litefs/data/cache.db "PRAGMA journal_mode = WAL;"
 npm run start
